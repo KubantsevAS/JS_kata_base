239d2a35835350a7ad926575636eba760408f365
KubantsevAS <92636316+KubantsevAS@users.noreply.github.com>
2022-02-03 01:28:03 +0300

    Update SummOf2Arrays.js

--- SummOf2Arrays.js
+++ SummOf2Arrays.js
@@ -9,6 +9,15 @@ function arrayPlusArray(arr1, arr2) {  // функция для сложения
   return summ; 
 }
 
+function arrayPlusArray(arr1, arr2) {  // функция для сложения элементов массива между собой
+  var summ = [];
+  for (let i = 0; i < arr1.length; i++){
+    summ[i] = arr1[i] + arr2[i];
+  }
+  return summ; 
+}
+alert(arrayPlusArray([1, 2, 5], [1, 4, 6]));
+
 function positiveSum(arr) {		// функция для сложения положительных элементов массива
   var summPos = 0;
   for ( let i = 0; i < arr.length; i++){
